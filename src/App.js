77a3612cac0ca2c4a5f963427e88ace4406abedc
charles <cmyweb@yahoo.com>
2022-05-11 13:47:29 +0100

    npm modules installed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+
+import { Box, Typography, Paper } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[url('')]">
+      {" "}
+      <Typography className=" ml-7 mt-7">Welcome to React Router!</Typography>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} /> */}
+      </Routes>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  important: "#root",
+  theme: {
+    screens: {
+      sm: { max: "639px" },
+      md: "640px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
